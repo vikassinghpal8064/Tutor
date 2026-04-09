fc49082e5f560170f37df4386eaeeaaec9d264fd
vikas <vikassingh8064@gmail.com>
2026-04-09 11:56:57 +0530

    now finally getting the object as response from gemini but earlier only empty object i am getting

--- Routes/user.js
+++ Routes/user.js
@@ -1,18 +1,51 @@
-const express= require("express");
-const app= express();
- const router= express.Router();
-
-
-
-router.get("/students/:type",async(req,res,next)=>{
-try{
-let {type}= req.params;
-console.log(type);
-res.send({message:"route is working fine"});
-}
-catch(err){
+const express = require("express");
+const env = require("dotenv").config();
+//const app= express();
+const router = express.Router();
+const { GoogleGenAI } = require("@google/genai");
+//console.log(process.env.GEMINI_API_KEY);
+//const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const ai = new GoogleGenAI({});
+router.get("/students/:type", async (req, res, next) => {
+  try {
+    let { type } = req.params;
+    console.log(type);
+    res.send({ message: "route is working fine" });
+  } catch (err) {
     console.log(err);
-}
-})
+  }
+});
 
-module.exports=router;
+router.post("/user/query", async (req, res, next) => {
+  
+  let { query } = req.body;
+  console.log(query);
+  console.log(typeof query);
+
+  try {
+    async function textgeneration() {
+        let response=null;
+      if (query != null) {
+       response = await ai.models.generateContent({
+          model: "gemini-3-flash-preview",
+          contents: query,
+        });
+        //const result = await model.generateContent(query);
+        //response =result.response;
+        console.log(response.text); // Note the function call .text()
+        return response.text;
+      }
+    }
+
+   let result = await textgeneration();
+    if (result == null) {
+      res.send({ message: "something went wrong with the generating text" });
+    } else {
+      res.send({ message: result });
+    }
+  } catch (err) {
+    res.send({ message: "you got the error " + err });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -4,25 +4,11 @@ const path=require("path");
 const user= require("./Routes/user");
 const env= require('dotenv').config();
 
-// Ensure you have installed: npm install @google/genai
-// Ensure you have installed: npm install @google/generative-ai
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
-async function textgeneration() {
-  // getGenerativeModel is the standard method for this SDK
-  const model = genAI.getGenerativeModel({ model: "gemini-3-flash-preview" });
-
-  const result = await model.generateContent("Explain AI in 5 words");
-  const response = await result.response;
-  console.log(response.text()); // Note the function call .text()
-}
-
-textgeneration();
-
 
 const PORT= process.env.PORT || 8080;
+app.use(express.json());// its a middle ware to parse json;
+app.use(user);
+
 
 app.listen(PORT,()=>{
 console.log("i am listining at the port "+PORT)
